23b885bacd4182cd1f92a172b7ad495cd4bec19d
Taanveer22 <taanveer469@gmail.com>
2025-07-21 12:56:11 +0600

    alhadulliah full project rechecked

---
+++ src/components/Loader.jsx
@@ -0,0 +1,20 @@
+// loader componet---------------------------
+/* eslint-disable react/prop-types */
+
+import { ScaleLoader } from "react-spinners";
+
+const Loader = () => {
+  return (
+    <div
+      className={` min-h-[calc(100vh-116px)]
+      flex 
+      flex-col 
+      justify-center 
+      items-center `}
+    >
+      <ScaleLoader size={100} color="#F92FD3" />
+    </div>
+  );
+};
+
+export default Loader;
