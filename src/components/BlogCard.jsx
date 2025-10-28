3283000794d16200484232628debaa1f1c1b192d
Taanveer22 <taanveer469@gmail.com>
2025-10-29 03:17:00 +0600

    alhamdulliah bookmarks deleted from page

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,26 +1,38 @@
 import { Link } from "react-router-dom";
 import placeHolderImage from "../assets/404.jpg";
-const BlogCard = ({ blog }) => {
+import { FaRegTrashCan } from "react-icons/fa6";
+
+const BlogCard = ({ blog, deletable, handleDeleteBookmark }) => {
   // console.log(blog);
   const { id, cover_image, title, description, published_at } = blog;
   return (
-    <div className="max-w-sm mx-auto group hover:no-underline focus:no-underline  transition p-3 border-2 border-primary hover:border-secondary hover:scale-105">
-      <Link to={`/blogdetail/${id}`}>
-        <img
-          role="presentation"
-          className="object-cover w-full rounded h-44 bg-gray-500 "
-          src={cover_image || placeHolderImage}
-        />
-        <div className="p-6 space-y-2">
-          <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
-            {title}
-          </h3>
-          <span className="text-xs text-gray-400 ">
-            {new Date(published_at).toLocaleDateString()}
-          </span>
-          <p>{description}</p>
-        </div>
-      </Link>
+    <div className="flex relative">
+      <div className="max-w-sm mx-auto group hover:no-underline focus:no-underline  transition p-3 border-2 border-primary hover:border-secondary hover:scale-105">
+        <Link to={`/blogdetail/${id}`}>
+          <img
+            role="presentation"
+            className="object-cover w-full rounded h-44 bg-gray-500 "
+            src={cover_image || placeHolderImage}
+          />
+          <div className="p-6 space-y-2">
+            <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+              {title}
+            </h3>
+            <span className="text-xs text-gray-400 ">
+              {new Date(published_at).toLocaleDateString()}
+            </span>
+            <p>{description}</p>
+          </div>
+        </Link>
+        {deletable && (
+          <div
+            onClick={() => handleDeleteBookmark(id)}
+            className="absolute -top-5 -right-5 bg-red-500 p-3 rounded-full"
+          >
+            <FaRegTrashCan size={30}></FaRegTrashCan>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/Bookmarks.jsx
+++ src/pages/Bookmarks.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getFromLocalStorage } from "../utilities";
+import { deleteFromLocalStorage, getFromLocalStorage } from "../utilities";
 import BlogCard from "../components/BlogCard";
 
 const Bookmarks = () => {
@@ -9,10 +9,21 @@ const Bookmarks = () => {
     const storedBlogs = getFromLocalStorage();
     setBlogs(storedBlogs);
   }, []);
+
+  const handleDeleteBookmark = (id) => {
+    deleteFromLocalStorage(id);
+    const storedBlogs = getFromLocalStorage();
+    setBlogs(storedBlogs);
+  };
   return (
     <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 w-11/12 mx-auto my-12">
       {blogs.map((blog) => (
-        <BlogCard blog={blog}></BlogCard>
+        <BlogCard
+          blog={blog}
+          key={blog.id}
+          deletable={true}
+          handleDeleteBookmark={handleDeleteBookmark}
+        ></BlogCard>
       ))}
     </div>
   );
